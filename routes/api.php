7fcff6b4a54d6d40b8380817a412507c866e3fec
Kenet Picado <kenetpicado1@gmail.com>
2023-02-12 21:57:40 -0600

    update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\GradeController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('v1/grades', GradeController::class);
+
+Route::post('test', TestController::class);
