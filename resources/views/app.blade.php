57fdd5ac8cbc2cc7092be6ca91d5f88a29339bdb
Kenet Picado <kenetpicado1@gmail.com>
2023-08-12 10:24:09 -0600

    integration is working

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\GradeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+Route::redirect('/', 'grades');
+
+Route::resource('grades', GradeController::class);
